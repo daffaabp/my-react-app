88512519bae3c97f99d97e3f382c8c63ca58022a
daffaabp <daffa.budi2003@gmail.com>
2024-03-23 14:28:26 +0700

    event handling form login

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -2,13 +2,25 @@ import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
 
 const FormLogin = () => {
+  // Handle Klik untuk Login
+  const handleLogin = (e) => {
+    e.preventDefault();
+    localStorage.setItem("email", e.target.email.value);
+    localStorage.setItem("password", e.target.password.value);
+    // console.log(e.target.email.value);
+    // console.log(e.target.password.value);
+    // console.log("login");
+    window.location.href = "/products";
+  };
+
+
   return (
-    <form action="">
+    <form onSubmit={handleLogin}>
 
       <InputForm label="Email" type="email" placeholder="example@gmail.com" name="email" />
       <InputForm label="Password" type="password" placeholder="*****" name="password" />
            
-      <Button classname="bg-blue-600 w-full">Login</Button>
+      <Button classname="bg-blue-600 w-full" type="submit">Login</Button>
     </form>
   );
 }
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,6 @@
+import { Fragment } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
+import Button from "../components/Elements/Button";
 
 const products = [
   {
@@ -24,22 +26,40 @@ const products = [
   }
 ];
 
+// Tangkap Email yang ada pada LocalStorage
+const email = localStorage.getItem('email');
+
 
 const ProductsPage = () => {
-  return (
-    <div className="flex justify-center py-5">
-     
-    {products.map((product) => (
-      <CardProduct key={product.id}>
-        <CardProduct.Header image={product.image} />
-        <CardProduct.Body name={product.name}>
-          {product.description}
-        </CardProduct.Body>
-        <CardProduct.Footer price={product.price} />
-      </CardProduct>
-    ))}
 
-    </div>
+  // Buat handle untuk Logout
+  const handleLogout = () => {
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+    window.location.href = "/login";
+  }
+
+  return (
+    <Fragment>
+
+      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
+      {email}
+      <Button classname="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
+      </div>
+
+      <div className="flex justify-center py-5">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        ))}
+      </div>
+
+    </Fragment>
   );
 }
 
